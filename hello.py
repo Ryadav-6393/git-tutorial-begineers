c150cdd3b1eed94a852081d9e9da04c98a0f99b5
Ryadav-6393 <rahulyadavhani@gmail.com>
2025-11-13 22:06:04 +0530

    Desidn completed

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # my  name is python program
 import pandas as pd
 import numpy as np
+import sklearn as sk
 
 print("hello world")
+print("This is my first python program")
+print("I am learning python programming")
